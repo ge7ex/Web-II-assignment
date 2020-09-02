fc8fe236278f336a6e9125af1e32264998903b22
ge7ex <68490494+ge7ex@users.noreply.github.com>
2020-09-02 13:02:38 +0700

    assignment 3,assignment 4

---
+++ assignment 4/js/shop.js
@@ -0,0 +1,19 @@
+var customername = "chaky";
+var price = 10;
+var quantity = 3;
+var totalPrice = price * quantity;
+var products = [
+			"Pizza",
+			"Naan",
+			"Sourdough"
+			"Foccacia"
+			"Iphone X"
+			];
+var price = [40,10,20,80,1000];
+var productstext = ""
+var productsElement = document.getElementById("product-list");
+
+productstext=productstext + "<li class='list-group-item'>" + product[0]+"<span class = 'badge'>$ + price[0] + "</li>";
+productElement.innerHTML = productstext;
+document.getElementById("customer-name").innerHTML = "Chaky";
+document.getElementById("price").innerHTML = "$" + totalPrice;
